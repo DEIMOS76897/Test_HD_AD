5449d7ae2d8ada43c321507a366d559f0197c978
DEIMOS76897 <158823757+DEIMOS76897@users.noreply.github.com>
2026-05-24 18:52:19 +0300

    booking.py

--- handlers/booking.py
+++ handlers/booking.py
@@ -2,14 +2,16 @@ from datetime import date, datetime, time
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery,Message
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from db.requests import get_lessons, get_free_times, get_or_create_user, get_lesson_by_id, create_record
 from keyboards.booking_kb import lessons_keyboard, times_keyboard
 from keyboards.calendar_kb import get_calendar
 from keyboards.menu import main_menu
 from states.booking_state import BookingState
+from sqlalchemy import select
+from db.models import Record
 
 router=Router()
 @router.callback_query(F.data=="start_booking")
@@ -111,3 +113,59 @@ async def choose_time(callback:CallbackQuery,state:FSMContext,session:AsyncSessi
         f"Время: {selected_time.strftime('%H:%M')}\n",reply_markup=main_menu()
     )
     await callback.answer()
+
+@router.callback_query(F.data == "my_records")
+async def my_records(callback: CallbackQuery, session: AsyncSession):
+    user = await get_or_create_user(session, tg_id=callback.from_user.id, user_name=callback.from_user.username or callback.from_user.full_name)
+    brn = select(Record).where(Record.user_id == user.id).order_by(Record.date, Record.time)
+    a = await session.execute(brn)
+    b = a.scalars().all()
+    if not b:
+        text = "У вас нет записей lol"
+    else:
+        lines = ["Ваши записи:"]
+        for rec in b:
+            lesson = await get_lesson_by_id(session, rec.lesson_id)
+            lines.append(f"{lesson.name} - {rec.date.strftime('%d.%m.%Y')} в {rec.time.strftime('%H:%M')}")
+        linej = "\n".join(lines)
+    await callback.message.answer(linej, reply_markup=main_menu())
+    await callback.answer()
+
+@router.callback_query(F.data == "cancel_record_menu")
+async def cancel_record_menu(callback: CallbackQuery, session: AsyncSession):
+    user_cn = await get_or_create_user(session, tg_id=callback.from_user.id, user_name=callback.from_user.username or callback.from_user.full_name)
+    ffbd = select(Record).where(Record.user_id == user_cn.id).order_by(Record.date, Record.time)
+    c = await session.execute(ffbd)
+    d = c.scalars().all()
+    if not d:
+        await callback.message.answer("У вас нет записей1", reply_markup=main_menu())
+        await callback.answer()
+        return
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=f"{ (await get_lesson_by_id(session, rec.lesson_id)).name } {rec.date.strftime('%d.%m.%Y')} {rec.time.strftime('%H:%M')}", callback_data=f"cancel_{rec.id}")]
+        for rec in d])
+    await callback.message.answer("Выберите запись для отмены", reply_markup=keyboard)
+    await callback.answer()
+@router.callback_query(F.data.startswith("cancel_"))
+async def cancel_record(callback: CallbackQuery, session: AsyncSession):
+    #пу пу пу пу
+    record_id = int(callback.data.split("_")[1])
+    user = await get_or_create_user(session, tg_id=callback.from_user.id, user_name=callback.from_user.username or callback.from_user.full_name)
+    g = (await session.execute(select(Record).where(Record.id == record_id, Record.user_id == user.id))).scalar_one_or_none()
+    if g:
+        await session.delete(g)
+        await session.commit()
+        await callback.answer()
+        await callback.message.answer("Запись отменена", reply_markup=main_menu())
+    else:
+        await callback.answer("Запись не найдена", show_alert=True)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
